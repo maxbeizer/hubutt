0c1f77ae06394ac13f2f34eed01ad83b8ee0f0de
max beizer <max.beizer@gmail.com>
2021-07-14 17:06:45 -0500

    Add progress on file parsing

--- .github/bin/load_help.rb
+++ .github/bin/load_help.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 # coding:utf-8
 
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
-puts 'Hello world'
+SCRIPT_PATH = 'scripts/'
+@all_help = ''
+
+Dir.foreach(SCRIPT_PATH) do |filename|
+  next if filename.start_with?('.')
+
+  File.open(SCRIPT_PATH + filename, 'r') do |f|
+    contents = f.read
+    contents.each_line do |line|
+      @all_help << line.sub(/^#\s*/, '') if line.match(/^#\s*hubot/)
+    end
+  end
+end
+
+puts @all_help
