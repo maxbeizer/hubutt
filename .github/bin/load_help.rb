096d290f93b8a02a7540313fded7350725100c06
max beizer <max.beizer@gmail.com>
2021-07-15 15:14:20 -0500

    Add the file name and make it csv

--- .github/bin/load_help.rb
+++ .github/bin/load_help.rb
@@ -1,18 +1,32 @@
+# frozen_string_literal: true
 #!/usr/bin/env ruby
-# coding:utf-8
 
 SCRIPT_PATH = 'scripts/'
-@all_help = ''
+
+# Convert lines of a script into output
+class ScriptLineProcessor
+  attr_reader :all_help
+
+  def initialize
+    @all_help = +''
+  end
+
+  def call(line, filename)
+    return unless line.match(/^#\s*hubot/)
+
+    command = line.sub(/^#\s*/, '').chomp
+    @all_help << [command, filename].join(',').concat(',')
+  end
+end
+
+@processor = ScriptLineProcessor.new
 
 Dir.foreach(SCRIPT_PATH) do |filename|
   next if filename.start_with?('.')
 
-  File.open(SCRIPT_PATH + filename, 'r') do |f|
-    contents = f.read
-    contents.each_line do |line|
-      @all_help << line.sub(/^#\s*/, '') if line.match(/^#\s*hubot/)
-    end
+  File.open(SCRIPT_PATH + filename, 'r') do |file|
+    file.read.each_line { |line| @processor.call(line, filename) }
   end
 end
 
-puts @all_help
+puts @processor.all_help
